90ee2a6412071b4d340ee1ff6977ed1becd46597
milos <webad012@gmail.com>
2014-04-29 03:18:38 +0200

    osnova igre odradjena

--- Assets/Scripts/MainGameController.cs
+++ Assets/Scripts/MainGameController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 [System.Serializable]
 public class Boundary
@@ -17,30 +18,57 @@ public class Boundary
 public class PicUps
 {
 	public GameObject pickUp1;
+	public GameObject pickUp2;
+	public GameObject pickUp3;
 }
 
 public class MainGameController : MonoBehaviour 
 {
 	public Boundary boundary;
 	public PicUps pickUps;
-	//public GameObject pickUp;
 
 	private int numberOfPickUps = 50;
 	private List<GameObject> pickUpList = new List<GameObject>();
+	Dictionary<string, int> pickUpDisplayList = new Dictionary<string, int> ();
+	private Vector2 scrollPosition = Vector2.zero;
 
 	// Use this for initialization
 	void Start () 
 	{
+		GeneratePickUps ();
+		GeneratePickUpsDisplayList ();
+	}
+
+	void GeneratePickUps()
+	{
 		for (int i= 0; i<numberOfPickUps; i++) 
 		{
 			Vector3 instantiatePosition = GenerateRandomPosition();
-			Quaternion instantiateRotation = Quaternion.identity;
-
-			GameObject pickUpObject = (GameObject)Instantiate(pickUps.pickUp1, instantiatePosition, instantiateRotation);
+			//Quaternion instantiateRotation = Quaternion.identity;
+			//Quaternion instantiateRotation = Random.rotation;
+			//Quaternion instantiateRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), 0);
+			
+			float randomPickup = UnityEngine.Random.value;
+			GameObject pickUpObject;
+			if(randomPickup < 0.3f)
+			{
+				//pickUpObject = (GameObject)Instantiate(pickUps.pickUp1, instantiatePosition, instantiateRotation);
+				pickUpObject = (GameObject)Instantiate(pickUps.pickUp1);
+			}
+			else if(randomPickup < 0.6f)
+			{
+				//pickUpObject = (GameObject)Instantiate(pickUps.pickUp2, instantiatePosition, instantiateRotation);
+				pickUpObject = (GameObject)Instantiate(pickUps.pickUp2);
+			}
+			else
+			{
+				//pickUpObject = (GameObject)Instantiate(pickUps.pickUp3, instantiatePosition, instantiateRotation);
+				pickUpObject = (GameObject)Instantiate(pickUps.pickUp3);
+			}
+			pickUpObject.transform.position = instantiatePosition;
 			pickUpObject.rigidbody.freezeRotation = true;
 			pickUpObject.rigidbody.drag = 4.0f;
-			pickUpList.Add((pickUpObject));
-			//pickUpList.Add((GameObject)Instantiate(pickUp, instantiatePosition, instantiateRotation));
+			pickUpList.Add(pickUpObject);
 		}
 	}
 
@@ -48,40 +76,48 @@ public class MainGameController : MonoBehaviour
 	{
 		float randomX = 0;
 		float randomZ = 0;
-		randomX = Random.Range(boundary.xMin, boundary.xMax);
-		randomZ = Random.Range(boundary.zMin, boundary.zMax);
-        
-		/*bool posOk = false;
-		while(posOk == false)
-		{
-			randomX = Random.Range(boundary.xMin, boundary.xMax);
-			randomZ = Random.Range(boundary.zMin, boundary.zMax);
-			
-			if(randomX >= -80 && randomX <= -93 && randomZ >= -105 && randomZ <= -120)
-            {
-				posOk = false;
-			}
-			else
-			{
-				posOk = true;
-			}
-        }*/
+		randomX = UnityEngine.Random.Range(boundary.xMin, boundary.xMax);
+		randomZ = UnityEngine.Random.Range(boundary.zMin, boundary.zMax);
 
 		return new Vector3(randomX, boundary.yMax, randomZ);
 	}
-	
-	// Update is called once per frame
-    void Update () 
+
+	void OnGUI()
 	{
-		//Debug.Log(pickUpList[0].transform.rigidbody.ToString());
-		/*foreach (GameObject pu in pickUpList) 
+		int labelHeight = 20;
+		int labelWidth = 150;
+		scrollPosition = GUI.BeginScrollView (new Rect(0, 0, Screen.width, 100), scrollPosition, new Rect(0, 0, Screen.width, pickUpDisplayList.Count*labelHeight));
+		int count = 0;
+		foreach (KeyValuePair<string, int> pud in pickUpDisplayList) 
 		{
-			if(pu.rigidbody.position.x >= -80 && pu.rigidbody.position.x <= -93
-			   && pu.rigidbody.position.z >= -105 && pu.rigidbody.position.z <= -120)
+			GUI.Label(new Rect(0, count*labelHeight, labelWidth, labelHeight), count + ": " + pud.Key + " x" + pud.Value.ToString());
+			count++;
+		}
+		GUI.EndScrollView ();
+	}
+
+	void GeneratePickUpsDisplayList()
+	{
+		pickUpDisplayList.Clear ();
+
+		foreach (GameObject pu in pickUpList) 
+		{
+			if(pickUpDisplayList.ContainsKey(pu.name))
 			{
-				Debug.Log(pu.transform.rigidbody.ToString());
-				pu.transform.position = GenerateRandomPosition();
+				pickUpDisplayList[pu.name] += 1;
 			}
-		}*/
+
+			if(!pickUpDisplayList.ContainsKey(pu.name))
+			{
+				pickUpDisplayList.Add(pu.name, 1);
+			}
+		}
+	}
+
+	public void PickUpSelected(GameObject pickUpObject)
+	{
+		pickUpList.Remove (pickUpObject);
+		Destroy (pickUpObject);
+		GeneratePickUpsDisplayList ();
 	}
 }
--- Assets/Scripts/PickUpController.cs
+++ Assets/Scripts/PickUpController.cs
@@ -3,8 +3,25 @@ using System.Collections;
 
 public class PickUpController : MonoBehaviour 
 {
+	private MainGameController gameController;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) 
+		{
+			gameController = gameControllerObject.GetComponent<MainGameController>();
+			
+		} else 
+		{
+			Debug.Log("Cannot find 'GameController' script.");
+			
+		}
+	}
+
 	void OnMouseDown()
 	{
-		Destroy (gameObject);
+		//Destroy (gameObject);
+		gameController.PickUpSelected(gameObject);
 	}
 }
