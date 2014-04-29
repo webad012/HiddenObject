a9b990211309a612412494b3aaad28f39868728b
milos <webad012@gmail.com>
2014-04-30 01:49:46 +0200

    ima 14 razlicitih objekata, neki su za skupljanje, vecina nije

--- Assets/Scripts/MainGameController.cs
+++ Assets/Scripts/MainGameController.cs
@@ -17,12 +17,20 @@ public class Boundary
 [System.Serializable]
 public class PicUps
 {
-	public GameObject pickUp1;
-	public GameObject pickUp2;
-	public GameObject pickUp3;
-	public GameObject pickUp4;
-	public GameObject pickUp5;
-	public GameObject pickUp6;
+	public GameObject android;
+	public GameObject framedPicture;
+	public GameObject m9;
+	public GameObject oilDrum;
+	public GameObject tank;
+	public GameObject enterprise;
+	public GameObject heart;
+	public GameObject iphone;
+	public GameObject boxes;
+	public GameObject stopSign;
+	public GameObject sword;
+	public GameObject bullet;
+	public GameObject katana;
+	public GameObject boat;
 }
 
 [System.Serializable]
@@ -40,8 +48,10 @@ public class MainGameController : MonoBehaviour
 	public GUIText loadingText;
 	public GUIText timerText;
 
-	private int numberOfPickUps = 50;
+	private int numberOfNonPickUps = 40;
+	private int numberOfPickUps = 10;
 	private List<GameObject> pickUpList = new List<GameObject>();
+	private List<GameObject> nonPickUpList = new List<GameObject>();
 	Dictionary<string, int> pickUpDisplayList = new Dictionary<string, int> ();
 	private Vector2 scrollPosition = Vector2.zero;
 
@@ -68,48 +78,119 @@ public class MainGameController : MonoBehaviour
             
         }
 
+		GenerateNonPickUps ();
 		GeneratePickUps ();
 		GeneratePickUpsDisplayList ();
+	}
+
+	GameObject InstantiatePickUpObject()
+	{
+		float randomPickup = UnityEngine.Random.value;
+		GameObject pickUpObject;
+		if(randomPickup < 0.07f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.android);
+		}
+		else if(randomPickup < 0.14f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.framedPicture);
+		}
+		else if(randomPickup < 0.21f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.m9);
+		}
+		else if(randomPickup < 0.28f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.oilDrum);
+		}
+		else if(randomPickup < 0.35f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.tank);
+		}
+		else if(randomPickup < 0.42f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.enterprise);
+		}
+		else if(randomPickup < 0.49f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.heart);
+		}
+		else if(randomPickup < 0.56f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.iphone);
+		}
+		else if(randomPickup < 0.63f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.boxes);
+		}
+		else if(randomPickup < 0.70f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.stopSign);
+		}
+		else if(randomPickup < 0.77f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.sword);
+		}
+		else if(randomPickup < 0.84f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.bullet);
+		}
+		else if(randomPickup < 0.93f)
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.katana);
+		}
+		else
+		{
+			pickUpObject = (GameObject)Instantiate(pickUps.boat);
+		}
+
+		pickUpObject.name = ObjectNameParse(pickUpObject.name);
+		pickUpObject.transform.position = GenerateRandomPickUpPosition();
+		pickUpObject.transform.rotation = Quaternion.Euler(pickUpObject.transform.rotation.eulerAngles.x, UnityEngine.Random.Range(-90, 90), pickUpObject.transform.rotation.eulerAngles.z);
+		pickUpObject.rigidbody.freezeRotation = true;
+		pickUpObject.rigidbody.drag = 4.0f;
+
+		return pickUpObject;
+	}
+
+	void GenerateNonPickUps()
+	{
+		for (int i= 0; i<numberOfNonPickUps; i++) 
+		{
+			GameObject pickUpObject = InstantiatePickUpObject();
+			nonPickUpList.Add(pickUpObject);
+		}
 	}
 
 	void GeneratePickUps()
 	{
 		for (int i= 0; i<numberOfPickUps; i++) 
 		{
-			float randomPickup = UnityEngine.Random.value;
-			GameObject pickUpObject;
-			if(randomPickup < 0.17f)
-			{
-				pickUpObject = (GameObject)Instantiate(pickUps.pickUp1);
-			}
-			else if(randomPickup < 0.34f)
-			{
-				pickUpObject = (GameObject)Instantiate(pickUps.pickUp2);
-			}
-			else if(randomPickup < 0.51f)
-			{
-				pickUpObject = (GameObject)Instantiate(pickUps.pickUp3);
-            }
-			else if(randomPickup < 0.68f)
-			{
-				pickUpObject = (GameObject)Instantiate(pickUps.pickUp4);
-            }
-			else if(randomPickup < 0.85f)
-			{
-				pickUpObject = (GameObject)Instantiate(pickUps.pickUp5);
-            }
-            else
-            {
-				pickUpObject = (GameObject)Instantiate(pickUps.pickUp6);
-			}
+			GameObject pickUpObject = new GameObject();
 
-			pickUpObject.name = ObjectNameParse(pickUpObject.name);
-			pickUpObject.transform.position = GenerateRandomPickUpPosition();
-			pickUpObject.transform.rotation = Quaternion.Euler(pickUpObject.transform.rotation.eulerAngles.x, UnityEngine.Random.Range(0, 360), pickUpObject.transform.rotation.eulerAngles.z);
-			pickUpObject.rigidbody.freezeRotation = true;
-			pickUpObject.rigidbody.drag = 4.0f;
+			do
+			{
+				Destroy(pickUpObject);
+				pickUpObject = InstantiatePickUpObject();
+
+			} while(NonPickUpListContainsObjectByName(pickUpObject.name));
+
+			pickUpObject.tag = "PickUp";
 			pickUpList.Add(pickUpObject);
 		}
+	}
+
+	bool NonPickUpListContainsObjectByName(string objName)
+	{
+		for (int i= 0; i<nonPickUpList.Count; i++) 
+		{
+			if(String.Compare( nonPickUpList[i].name, objName) == 0)
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	Vector3 GenerateRandomPickUpPosition()
@@ -146,14 +227,6 @@ public class MainGameController : MonoBehaviour
 			float guiTime = Time.time - timerStartTime;
 			int timerTimeLeft = Mathf.CeilToInt(timerTimeout - guiTime);
 			timerText.text = "Time left: " + timerTimeLeft;
-			/*if(timerTimeSpent > loadingTimeout)
-			{
-				
-				foreach(GameObject pu in pickUpList)
-				{
-					pu.rigidbody.isKinematic = true;
-				}
-			}*/
 		}
 	}
 
@@ -164,8 +237,11 @@ public class MainGameController : MonoBehaviour
 			float loadingTimeSpent = Time.time - loadingStartTime;
 			if(loadingTimeSpent > loadingTimeout)
 			{
-
 				foreach(GameObject pu in pickUpList)
+				{
+					pu.rigidbody.isKinematic = true;
+				}
+				foreach(GameObject pu in nonPickUpList)
 				{
 					pu.rigidbody.isKinematic = true;
 				}
@@ -183,6 +259,10 @@ public class MainGameController : MonoBehaviour
 			if(allStopped)
 			{
 				foreach(GameObject pu in pickUpList)
+				{
+					pu.rigidbody.isKinematic = true;
+				}
+				foreach(GameObject pu in nonPickUpList)
 				{
 					pu.rigidbody.isKinematic = true;
 				}
@@ -219,9 +299,12 @@ public class MainGameController : MonoBehaviour
 
 	public void PickUpSelected(GameObject pickUpObject)
 	{
-		pickUpList.Remove (pickUpObject);
-		Destroy (pickUpObject);
-		GeneratePickUpsDisplayList ();
+		if (!NonPickUpListContainsObjectByName (pickUpObject.name)) 
+		{
+			pickUpList.Remove (pickUpObject);
+			Destroy (pickUpObject);
+			GeneratePickUpsDisplayList ();
+		}
 	}
 
 	string ObjectNameParse(string inName)
